7908b8eabb2a3f284699addd03e34655c419a660
小豆豆 <753610399@qq.com>
2018-07-22 10:17:27 +0800

    更新winsw1.9的下载地址

--- bin/start.bat
+++ bin/start.bat
@@ -14,7 +14,7 @@
 ::		start.bat 主脚本，下载地址：http://download.csdn.net/user/xiangyuecn
 ::		rolllog.vbs 配置文件日期替换更新脚本，下载地址：http://download.csdn.net/user/xiangyuecn
 ::		PsExec.exe 用system用户身份管理nginx，下载地址：http://technet.microsoft.com/en-us/sysinternals/bb896649.aspx
-::		winsw1.9.exe windows服务安装器，下载地址：http://download.java.net/maven/2/com/sun/winsw/winsw/ 配置介绍：https://kenai.com/projects/winsw/pages/ConfigurationSyntax
+::		winsw1.9.exe windows服务安装器，下载地址：http://central.maven.org/maven2/com/sun/winsw/winsw/1.9/ 配置介绍：https://github.com/kohsuke/winsw/blob/master/doc/xmlConfigFile.md
 ::	这几个文件组成，缺一不可
 
 @echo off
